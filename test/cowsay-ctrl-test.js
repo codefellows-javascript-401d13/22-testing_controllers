9bee815105ee8065a6a0e7ca083188b58d0ed4fb
GLSea1979 <builtbygl@gmail.com>
2017-03-22 14:37:48 -0700

    initial build

---
+++ test/cowsay-ctrl-test.js
@@ -0,0 +1,21 @@
+'use strict';
+
+require('./lib/test-setup.js');
+
+const angular = require('angular');
+const cowsay = require('cowsay-browser');
+
+describe('Cowsay Controller', function() {
+  beforeEach(() => {
+    angular.mock.module('cowsayApp');
+    angular.mock.inject($controller => {
+      this.cowsayCtrl = new $controller('CowsayController');
+    });
+  });
+
+  describe('initial properties', () => {
+    it('title property should equal Welcome to the farm', () => {
+      expect(this.cowsayCtrl.title).toBe('Welcome to the Farm!');
+    });
+  });
+});
