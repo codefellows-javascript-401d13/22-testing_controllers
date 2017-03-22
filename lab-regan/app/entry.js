3d5f086ca487d3a469f6180635b6e0f83dd33b2b
reganoneill <rkrotho@gmail.com>
2017-03-22 14:47:30 -0700

    lab setup complete

---
+++ lab-regan/app/entry.js
@@ -0,0 +1,60 @@
+'use strict';
+
+require('./scss/reset.scss');
+require('./scss/main.scss');
+
+const angular = require('angular');
+const cowsay = require('cowsay-browser');
+
+const cowsayLab = angular.module('cowsayLab', []);
+
+cowsayLab.controller('CowsayController', ['$log', CowsayController]);
+
+function CowsayController($log){
+  $log.debug('CowsayController');
+
+  this.title = 'Creature Creeps! Make Em Speak!';
+  this.history = [];
+
+  cowsay.list((err, cowfiles) => {
+    this.cowfiles = cowfiles;
+    this.current = this.cowfiles[0];
+  });
+
+  this.update = function(input){
+    $log.debug('cowsayCtrl.update');
+    return cowsay.say({ text: input, || 'Say wuuuut?!', f: this.current });
+  };
+
+  this.speak = function(input){
+    $log.debug('cowsayCtrl.speak');
+    this.spoken = this.update(input);
+    this.history.push(this.spoken);
+  };
+
+  this.undo = function(){
+    $log.debug('cowsayCtrl.undo');
+    this.history.pop();
+    this.spoken = this.history.pop() || '';
+  };
+};
+
+cowsayLab.controller('NavController', ['$log', NavController]);
+
+function NavController($log){
+  $log.debug('NavController');
+  this.routes = [
+    {
+      name: 'home',
+      url: '/home'
+    },
+    {
+      name: 'about',
+      url: '/about'
+    },
+    {
+      name: 'contact',
+      url: '/contact-us'
+    }
+  ];
+}
