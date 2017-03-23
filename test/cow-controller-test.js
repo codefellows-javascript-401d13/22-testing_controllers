1e89d5d832feec5912b4d16656711c5a897fe643
Nikko Pisciotti <nikko@bluelacesmusic.com>
2017-03-22 18:23:10 -0700

    oops forgot to push last changes, thanks jeff

--- test/cow-controller-test.js
+++ test/cow-controller-test.js
@@ -5,27 +5,56 @@ require('./lib/test-setup.js');
 const angular = require('angular');
 const cowsay = require('cowsay-browser');
 
-describe('Cowsay Controller', () => {
+describe('Cowsay Controller', function() {
   beforeEach( () => {
     angular.mock.module('mooMooApp');
     angular.mock.inject($controller => {
       this.cowsayCtrl = new $controller('CowsayController');
+      this.navCtrl = new $controller('NavController');
+    });
+  });
+
+  describe('initial properties', () => {
+    it('title should be cowcreator.io', () => {
+      expect(this.cowsayCtrl.title).toBe('Cow Creator');
     });
 
-    describe('initial properties', () => {
-      it('title should be cowcreator.io', () => {
-        expect(this.cowsayCtrl.title).toBe('cowcreator.io');
-      });
+    it('history property should be an empty array', () => {
+      expect(Array.isArray(this.cowsayCtrl.history)).toBe(true);
+    });
 
-      it('history property should be an empty array', () => {
-        expect(Array.isArray(this.cowsayCtrl.history)).toBe(true);
+    it('list of cowfiles should show proper cowfiles', () => {
+      cowsay.list((err, cowfiles) => {
+        expect(this.cowsayCtrl.cowfiles).toEqual(cowfiles);
       });
+    });
+  });
 
-      it('list of cowfiles should show proper cowfiles', () => {
-        cowsay.list((err, cowfiles) => {
-          expect(this.cowsayCtrl.cowfiles).toEqual(cowfiles);
-        });
-      });
+  describe('#update', () => {
+    it('should return a cow that says testing', () => {
+      let expected = cowsay.say({ text: 'testing', f: this.cowsayCtrl.current });
+      let testResult = this.cowsayCtrl.update('testing');
+      expect(testResult).toEqual(expected);
+    });
+  });
+
+  describe('#speak', () => {
+    it('should return a cow that says testing', () => {
+      let expected = cowsay.say({ text: 'testing', f: this.cowsayCtrl.current })
+      this.cowsayCtrl.speak('testing');
+      expect(this.cowsayCtrl.spoken).toEqual(expected);
+      expect(this.cowsayCtrl.history.length).toEqual(1);
+    });
+  });
+
+  describe('#undo', () => {
+    it('should modify our history array and return a cow that says testing', () => {
+      let expected = cowsay.say({ text: 'testing', f: this.cowsayCtrl.current });
+      this.cowsayCtrl.speak('testing');
+      this.cowsayCtrl.speak('test control');
+      this.cowsayCtrl.undo();
+      expect(this.cowsayCtrl.spoken).toEqual(expected);
+      expect(this.cowsayCtrl.history.length).toEqual(0);
     });
   });
 });
